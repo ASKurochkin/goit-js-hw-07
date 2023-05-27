3c83cacaf2837a6269b149c94168c6ed24163b1e
OLEKSANDR KUROCHKIN <kurochkinac@gmail.com>
2023-05-27 09:39:54 +0300

    Done home work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const markupGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", markupGallery);
+
+const onClick = (event) => {
+  event.preventDefault();
+
+  const largeImg = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${largeImg}" alt="${event.target.alt}">
+    </div>
+`);
+  instance.show();
+
+  document.addEventListener("keydown", handleEsc);
+
+  function handleEsc(event) {
+    if (event.key === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", handleEsc);
+    }
+  }
+};
+
+galleryEl.addEventListener("click", onClick);
+
+// console.log(galleryEl);
